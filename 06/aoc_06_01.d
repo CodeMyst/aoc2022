f8a5a331cd915f8edadbd5c8a4393ac39636a1ab
CodeMyst <code@myst.rs>
2022-12-06 13:18:54 +0100

    d06

---
+++ 06/aoc_06_01.d
@@ -0,0 +1,37 @@
+import std.stdio;
+import std.file;
+import std.array;
+import std.string;
+import std.algorithm;
+import std.container;
+import std.conv;
+
+void main()
+{
+    const res = readText("input").getStartOfPacketIndex();
+
+    writeln(res);
+}
+
+int getStartOfPacketIndex(string s)
+{
+    for (int start = 0; start < s.length; start++)
+    {
+        const end = start + 4;
+
+        if (end + 1 >= s.length) break;
+
+        const seq = s[start..end];
+
+        if (seq.isMarker()) return end;
+    }
+
+    return 0;
+}
+
+bool isMarker(string seq)
+{
+    const set = redBlackTree(seq.array());
+
+    return seq.length == set.length;
+}
